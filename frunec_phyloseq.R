6967c26c10adf945f085d4dd39e6cd94441d8736
aravenscraft <alison@pierid.local>
2015-07-11 12:38:56 -0700

    Added/updated some data files with 16s sequencing info, and updated the analysis script

--- frunec_phyloseq.R
+++ frunec_phyloseq.R
@@ -1,141 +1,107 @@
-# Example phyloseq analysis
+# Analysis of Costa Rican nectars and wild fruit: chemical and microbial survey!
 
-# R Vannette 
-# 5/21/15
+############################
+############################
+## PART 1: Bacterial OTUs ##
+############################
+############################
 
-#########################
-# Bacterial phyloseq ##
-#########################
-
-#install.packages("phyloseq")
+# load packages
 require(phyloseq)
 require(ape)
-#adding a line of code here
+library(ggplot2)
+
 
 #change this to the folder that contains the bacterial data
-setwd('~/Documents/2010 STANFORD/2015_3 summer/frunec')
+setwd('~/Documents/2010 STANFORD/2015_3 summer/frunec/CR_frunec')
 
-# OTU table
-biom<-import_biom("RMBL_otus_table_wTaxv2_fn.biom", header=T)
+# 16s OTU table
+samps <- read.table('~/Documents/2010 STANFORD/2015_3 summer/frunec/CR_frunec/frunec_16s_otutable.txt',row.names=1, header=T)
+mat <- data.matrix(samps, rownames.force = T)
+samps <- otu_table(mat, taxa_are_rows=T)
 
-# change column names to match those in the sample data and taxonomy file: these need to be UNIQUE and CONSISTENT
-colnames(biom)<-substring(colnames(biom),6,11)
+# 16s taxonomy
+tax <- read.csv('~/Documents/2010 STANFORD/2015_3 summer/frunec/CR_frunec/repset_tax_assignments.csv', row.names=1, header=T)
+taxmat <- as.matrix(tax,rownames.force=T)
+taxtab <- tax_table(taxmat)
 
-#colnames(biom)
-OTU = otu_table(biom, taxa_are_rows = TRUE)
+# 16s mapping file
+map <- read.csv('~/Documents/2010 STANFORD/2015_3 summer/frunec/CR_frunec/mapping_fn.csv', row.names=1, header=T)
+map <- sample_data(map)
 
+# 16s tree file
+tree <- read.tree('~/Documents/2010 STANFORD/2015_3 summer/frunec/CR_frunec/rep_set.tre')
 
-###
-# input taxonomy 
-# may need to modify this for other formats, but this is formatted for RDP output.
-rdptaxonomy<-read.table("~/Bioinformatics/MiSeq/2012_comb/prep/fixrank_otus_repsetOUT.fasta_classified.txt",sep="\t",
-                fill=FALSE, strip.white=T)
-
-###########
-#code to split up the rdp taxonomy string #
-# modify as necessary for your particular taxonomy file #
-########
-
-tax<-as.character(rdptaxonomy[-(1:6),])
-#head(tax)
-OTU.n<-NA
-#root<-NA
-Kingdom<-NA
-Phylum<-NA
-#Subphylum<-NA
-Class<-NA
-Order<-NA
-Family<-NA
-Genus<-NA
-#Species<-NA
-#Gen_ID<-NA
-
-
-linaean<-data.frame(OTU.n,Kingdom,Phylum,Class,Order,Family,Genus)
-
-for (i in 1:length(tax)){
-  
-  split.tax<-strsplit(tax[i],split=c(";"))
-  linaean[i,1]<-substr(split.tax[[1]][1], 1,30)
-  linaean[i,2]<-substr(split.tax[[1]][3], 1,30)
-  linaean[i,3]<-substr(split.tax[[1]][5], 1,30)
-  linaean[i,4]<-substr(split.tax[[1]][7], 1,30)
-  linaean[i,5]<-substr(split.tax[[1]][9], 1,30)
-  linaean[i,6]<-substr(split.tax[[1]][11], 1,30)
- # linaean[i,7]<-substr(split.tax[[1]][19], 1,30)
- # linaean[i,8]<-substr(split.tax[[1]][21], 1,30)
-  #linaean[i,9]<-substr(split.tax[[1]][23], 1,30)
-
-}
-####
-
-rownames(linaean)<- linaean$OTU.n
-#head(linaean)
-TAX<-tax_table(as.matrix(linaean))
+# glue them all together
+bacteria <- merge_phyloseq(samps,taxtab,map,tree)
 
 
 
-# Tree file
-setwd('~/Bioinformatics/MiSeq/2012_comb/')
+# Remove undesireable OTUs.
+head(tax_table(bacteria))
+table(tax$kingdom) # There are 18 archaeans.
+table(tax$phylum) # There are 342 OTUs with no assigned phylum.  I am removing them for now.
+table(tax$class) # There are 62 chloroplast OTUs
+levels(tax$order)
+levels(tax$family)
 
-tre<-read.tree("./prep/alignment/rep_set_tree.tre")
-# may need to relabel tips to match OTU names above. 
-#tre$tip.label<-paste( tre$tip.label, sep="")
+noeuks<-subset_taxa(bacteria, kingdom!="Unclassified")
+noeuks<-subset_taxa(noeuks, kingdom!="Archaea")
+noeuks<-subset_taxa(noeuks, phylum!="")
+noeuks<-subset_taxa(noeuks, class!="Chloroplast")
+noeuks<-subset_taxa(noeuks, family!="mitochondria")
 
-# mapping file with sample info
-setwd("~/Desktop/R files/2012/Jasper Ridge/")
-dat<-read.csv("merged_2012_JRBP_all.csv", na.strings=c("NA", ".", "#DIV/0!"))
-
-# files are merged by rowname, so make this consistent with above. 
-rownames(dat)<-dat$Flower.ID.Match
-samp<-sample_data(dat)
-
-
-########################
-###############
-# make phyloseq object 
-################
-
-myphy<-phyloseq(OTU, samp, TAX, tre)
-
-
-#head(tax_table(myphy))
-noeuks<-subset_taxa(myphy, Phylum!="Cyanobacteria")
-noeuks<-subset_taxa(noeuks, Kingdom!="Archaea")
-noeuks<-subset_taxa(noeuks, Kingdom!="")
-noeuks<-subset_taxa(noeuks, Phylum!="")
-noeuks<-subset_taxa(noeuks, Family!="mitochondria")
-noeuks<-subset_taxa(noeuks, Phylum!="unclassified_Root")
-noeuks<-subset_taxa(noeuks, Phylum!="unclassified_Bacteria")
-noeuks<-subset_taxa(noeuks, Phylum!="unclassified_Bacteria")
-noeuks<-subset_taxa(noeuks, Class!="Chloroplast")
-noeuks<-subset_taxa(noeuks, Class!="unclassified_Cyanobacteria/Chloroplast")
-noeuks<-subset_taxa(noeuks, Class!="unclassified_Cyanobacteria/Chloroplast")
 
 # can deal with negatives as you wish: either subtract out OTUs, subtract # of sequences, just look at them...
+
+
+IMPORTANT TO DO: look at the negative controls!  Subtract out likely contaminants.
+
+Negative control samples are:
+	pb: pcr blanks
+	eb: extraction blanks
+	water: water blanks
+	ctab: ctab blanks
+	sterile water blank: like it says.
+	
+	(cant remember the difference between water and sterile water blank; its written down in my field notes.)
+
+
+#colnames(samps)
+#otu <- otu_table(samps, taxa_are_rows = TRUE)
+
+
+sum(sample_sums(noeuks)) # total number of reads
+hist(sample_sums(noeuks), breaks=20)
+range(sample_sums(noeuks)) 
+
 
 ####################################
 #further data cleaning: check to see what you're getting rid of...
 ####################################
-# remove all samples with less than 50 total reads
-noeuks = prune_samples(sample_sums(noeuks)>=50, noeuks)
-
 zero_samples=which(taxa_sums(noeuks)==0)
 length(zero_samples)
 noeuks=prune_taxa(taxa_sums(noeuks)>0,noeuks)
+
 # remove taxa not seen more than 3 times in at least 1% of samples
 noeuks_f = filter_taxa(noeuks, function(x) sum(x > 3) > (0.01*length(x)), TRUE)
 dim(otu_table(noeuks))
 dim(otu_table(noeuks_f))
 
 
+
+# Rank abundance curve
+barplot(sort(taxa_sums(rare),TRUE,)[1:100],las=2,cex.axis=.7,ylab="No. Sequences",xlab="OTU",cex=0.5,main="Rank Abundance Plot for Top 100 OTUs")
+
+
 #####################################
 # Examine diversity patterns
 #####################################
-rare<-rarefy_even_depth(noeuks_f, rngseed=1, verbose=T)
-sample_data(rare)$month<-reorder.factor(sample_data(rare)$month, new.order=c("May", "June", "LJune"))
+rare <- rarefy_even_depth(noeuks_f, rngseed=1, verbose=T)
+sample_data(rare)$foodtype <- reorder.factor(sample_data(rare)$foodtype, new.order=c("fruit", "nectar", "control", "eb", "pb"))
 
-plot_richness(rare, x="Treatment", "month", measures=c("Observed", "Shannon", "InvSimpson"))+geom_boxplot()
+plot_richness(rare, x="species", measures=c("Observed", "Shannon", "InvSimpson"))+geom_boxplot()
+
 
 #####################################
 # Patterns of community composition
@@ -147,11 +113,54 @@ proportions = prune_samples(sample_sums(proportions)>=1, proportions)
 GPdist = phyloseq::distance(proportions, "bray")
 GP.ord <- ordinate(proportions, "NMDS", "bray", weighted=T)
 
-p1 = plot_ordination(proportions, GP.ord, color="LogBactCFU", shape="Treatment", 
+
+# ugh, I'm bad at ggplot.  can't figure out how to assign colors and shapes.
+
+p1 = plot_ordination(proportions, GP.ord, color="species", shape="foodtype", 
                      title="Bacterial OTUs NMDS (Bray-Curtis)")+theme_bw()+facet_wrap(~month)
 quartz()
 p1
 
 adonis(GPdist~month*Treatment,  as(sample_data(proportions), "data.frame"))
+
+
+
+
+
+###########################
+###########################
+### PART 2: Fungal OTUs ###
+###########################
+###########################
+
+# TO DO:
+Load the fungal OTU table here!
+
+
+
+
+
+
+
+###############################
+###############################
+### PART 3: Chemistry data! ###
+###############################
+###############################
+
+load('~/Documents/2010 STANFORD/2015_3 summer/frunec/CR_frunec/frunec_chem.Rdata', verbose=T)
+
+rownames(sample_data(rare))
+chem$SampleName <- paste('fn', chem$SampleName, sep='')
+
+## NB: these don't match because each of the chem samples has a corresponding "partner" sample that was sequenced.  Partners were usually from different flowers on the same plant, collected at the same time.
+
+# TO DO:
+Load the list of correpsonding partners, then merge the chemistry data into the phyloseq mapping file.
+
+
+
+
+
 
 
